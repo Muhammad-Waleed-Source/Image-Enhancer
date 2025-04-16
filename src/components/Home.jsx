fbe2e0b0cf8d57df4dcf90f3b26b41f304079443
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-04-16 14:42:11 +0500

    Image Enhancer API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,33 @@
-import React, { useState } from "react";
 import ImageUpload from "./ImageUpload";
 import ImagePreview from "./ImagePreview";
+import { useState } from "react";
+import { enhancedImageAPI } from "../utils/enhancedImageAPI";
 
 const Home = () => {
   const [uploadImage, setUploadImage] = useState(null);
   const [enhancedImage, setEnhancedImage] = useState(null);
-  const [loading, setLoading] = useState(null);
+  const [loading, setloading] = useState(false);
 
-  const uploadImageHandler = (file) => {
+  const UploadImageHandler = async (file) => {
     setUploadImage(URL.createObjectURL(file));
-    setLoading(true);
-    // calling API to enhance the image
+    setloading(true);
+    try {
+      const enhancedURL = await enhancedImageAPI(file);
+      setEnhancedImage(enhancedURL);
+      setloading(false);
+    } catch (error) {
+      console.log(error);
+      alert("Error while enhancing the image. Please try again later.");
+    }
   };
+
   return (
     <>
-      <ImageUpload uploadImageHandler={uploadImageHandler} />
+      <ImageUpload UploadImageHandler={UploadImageHandler} />
       <ImagePreview
         loading={loading}
         uploaded={uploadImage}
-        enhanced={enhancedImage}
+        enhanced={enhancedImage?.image}
       />
     </>
   );
